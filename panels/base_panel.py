4c8f7a7d5cef676fc1c85c5162f4413f12200100
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-13 16:49:12 +0300

    test

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -136,14 +136,7 @@ class BasePanel(ScreenPanel):
         self.current_extruder = self._printer.get_stat("toolhead", "extruder")
         self.control['temp_box'].pack_start(self.labels["%s_box" % self.current_extruder], True, 5, 5)
 
-        if self._printer.has_heated_bed():
-            heater_bed = self._gtk.Image("bed.svg", None, .4, .4)
-            self.labels['heater_bed'] = Gtk.Label(label="20 C")
-            # self.labels['heater_bed'].get_style_context().add_class("printing-info")
-            heater_bed_box = Gtk.Box(spacing=0)
-            heater_bed_box.pack_start(heater_bed, True, 5, 5)
-            heater_bed_box.pack_start(self.labels['heater_bed'], True, 3, 3)
-            self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
+       
 
 
     def activate(self):
