86dc21170087fc201978ffc856c50a0a72e2ed00
DmitriyAD <Alexandrovi4dd@gmail.com>
2022-04-14 12:18:43 +0300

    k

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -153,11 +153,7 @@ class BasePanel(ScreenPanel):
             heat_up = Gtk.Box(spacing=0)
             heat_up.pack_start(heat_up, True, 7, 7)
             heat_up.pack_start(self.labels['heat-up'], True, 3, 3)
-            self.control['temp_box'].pack_end(heat_up, True, 9, 9)
-        
-
-       
-
+            self.control['temp_box'].pack_end(heat_up, True, 7, 7)
 
     def activate(self):
         size = self.control['time_box'].get_allocation().width
@@ -192,6 +188,12 @@ class BasePanel(ScreenPanel):
     def process_update(self, action, data):
         if action != "notify_status_update" or self._printer is None:
             return
+
+        if self._printer.has_heat_up():
+            self.labels["heat-up"].set_label(
+                "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
+        for x in self._printer.get_tools():
+            self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))
 
         if self._printer.has_heated_bed():
             self.labels["heater_bed"].set_label(
