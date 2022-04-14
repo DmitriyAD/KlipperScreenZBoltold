a1251845c72d18c7828f36dc52dddb236306cc0a
DmitriyAD <Alexandrovi4dd@gmail.com>
2022-04-14 12:43:56 +0300

    k

--- ks_includes/printer.py
+++ ks_includes/printer.py
@@ -195,7 +195,6 @@ class Printer:
         heaters = []
         if self.has_heated_bed():
             heaters.append("heater_bed")
-            heaters.append("heat-up")
         for h in self.get_config_section_list("heater_generic "):
             heaters.append(h)
         for h in self.get_config_section_list("temperature_sensor "):
