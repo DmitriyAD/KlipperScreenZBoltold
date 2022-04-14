a6641d8d4744e12fe997a63feb29baed47b73d59
DmitriyAD <Alexandrovi4dd@gmail.com>
2022-04-14 12:25:50 +0300

    1

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -146,14 +146,6 @@ class BasePanel(ScreenPanel):
             heater_bed_box.pack_start(self.labels['heater_bed'], True, 3, 3)
             self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
             
-        if self._printer.has_heat_up():
-            heat_up = self._gtk.Image("heat-up.svg", None, .4, .4)
-            self.labels['heat-up'] = Gtk.Label(label="20 C")
-            # self.labels['heater_bed'].get_style_context().add_class("printing-info")
-            heat_up = Gtk.Box(spacing=0)
-            heat_up.pack_start(heat_up, True, 3, 3)
-            heat_up.pack_start(self.labels['heat-up'], True, 3, 3)
-            self.control['temp_box'].pack_end(heat_up, True, 3, 3)
 
     def activate(self):
         size = self.control['time_box'].get_allocation().width
