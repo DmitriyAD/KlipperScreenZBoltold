d5624c16a6bacab32c4b01c1d4cbba581767848a
Transistor427 <427departament@gmail.com>
2022-04-14 12:07:57 +0300

    s

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -151,9 +151,9 @@ class BasePanel(ScreenPanel):
             self.labels['heat-up'] = Gtk.Label(label="20 C")
             # self.labels['heater_bed'].get_style_context().add_class("printing-info")
             heat_up = Gtk.Box(spacing=0)
-            heat_up.pack_start(heater_bed, True, 7, 7)
+            heat_up.pack_start(heat_up, True, 7, 7)
             heat_up.pack_start(self.labels['heat-up'], True, 3, 3)
-            self.control['temp_box'].pack_end(heater_bed_box, True, 7, 7)
+            self.control['temp_box'].pack_end(heat_up, True, 7, 7)
         
 
        
