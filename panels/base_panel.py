a6ae7cd997eac6148a7a98229bd40482c80439ff
DmitriyAD <Alexandrovi4dd@gmail.com>
2022-04-14 14:20:34 +0300

    tetet

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -195,10 +195,6 @@ class BasePanel(ScreenPanel):
             self.labels["heater_bed"].set_label(
                 "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
         for x in self._printer.get_tools():
-            self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))
-        self.labels["heat-up"].set_label(
-                "%02d°" % round(self._printer.get_dev_stat("heat-up", "temperature"))) 
-        for x in self._printer.get_tools():
             self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))           
     
         if "toolhead" in data and "extruder" in data["toolhead"]:
