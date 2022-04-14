ebc86ec9cf28d8ff5c3784f1f2defe825981d502
DmitriyAD <Alexandrovi4dd@gmail.com>
2022-04-14 14:18:25 +0300

    rr

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -194,10 +194,12 @@ class BasePanel(ScreenPanel):
         if self._printer.has_heated_bed():
             self.labels["heater_bed"].set_label(
                 "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
-            self.labels["heater_bed"].set_label(
-                "%02d°" % round(self._printer.get_dev_stat("control_grid", "temperature")))
         for x in self._printer.get_tools():
             self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))
+        self.labels["heat-up"].set_label(
+                "%02d°" % round(self._printer.get_dev_stat("control_grid", "temperature"))) 
+        for x in self._printer.get_tools():
+            self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))           
     
         if "toolhead" in data and "extruder" in data["toolhead"]:
             if data["toolhead"]["extruder"] != self.current_extruder:
