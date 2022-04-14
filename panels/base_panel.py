ab1dc6196a0749ef2356d1cf7ffb9a0d4c843046
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-14 17:10:40 +0300

    xx

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -195,9 +195,9 @@ class BasePanel(ScreenPanel):
                 "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
         for x in self._printer.get_tools():
             self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature"))) 
-            self.labels[x] = self._gtk.ButtonImage("heat-up")
-            # self.labels["heat-up"].set_label(
-            #     "%02d°" % round(self._printer.get_dev_stat(x, "temperature")))     
+            # self.labels[x] = self._gtk.ButtonImage("heat-up")
+            self.labels["heat-up"].set_label[x](
+                "%02d°" % round(self._printer.get_dev_stat(x, "temperature")))     
                    
     
         if "toolhead" in data and "extruder" in data["toolhead"]:
