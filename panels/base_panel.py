75ae0acba16084a4dc5963b93d38c493696b2ad9
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-15 10:55:40 +0300

    test

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from cProfile import label
 import datetime
+from re import X
 import gi
 import logging
 
@@ -137,7 +138,8 @@ class BasePanel(ScreenPanel):
         self.current_extruder = self._printer.get_stat("toolhead", "extruder")
         self.control['temp_box'].pack_start(self.labels["%s_box" %  self.current_extruder], True, 5, 5)
 
-        if self._printer.has_heated_bed():
+        # if self._printer.has_heated_bed():
+        if  self._printer.get_tools():    
             heater_bed = self._gtk.Image("heat-up.svg", None, .4, .4)
             self.labels['heat-up'] = Gtk.Label(label="25 C")
             # self.labels['heat_up'].get_style_context().add_class("printing-info")
@@ -145,7 +147,7 @@ class BasePanel(ScreenPanel):
             heater_bed_box.pack_start(heater_bed, True, 5, 5)
             heater_bed_box.pack_start(self.labels['heat-up'], True, 3, 3)
             self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
-        
+        if self._printer.has_heated_bed():
             heater_bed = self._gtk.Image("bed.svg", None, .4, .4)
             self.labels['heater_bed'] = Gtk.Label(label="20 C")
             # self.labels['heater_bed'].get_style_context().add_class("printing-info")
@@ -194,7 +196,7 @@ class BasePanel(ScreenPanel):
             self.labels["heater_bed"].set_label(
                 "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
         for x in self._printer.get_tools():
-            self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))      
+            self.labels['heat-up'].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))      
                    
     
         if "toolhead" in data and "extruder" in data["toolhead"]:
