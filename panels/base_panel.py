bb84695660935d66aeaed9c1372de3f57d01e753
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-15 11:07:33 +0300

    f

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -197,7 +197,7 @@ class BasePanel(ScreenPanel):
                 "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
         for x in self._printer.get_tools():
             self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))  
-         if action != "notify_status_update" or self._printer is None:
+        if action != "notify_status_update" or self._printer is None:
             return
         if self._printer.has_heated_bed():
             self.labels["heat-up"].set_label(
