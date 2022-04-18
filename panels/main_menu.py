5914dba53230e9db812e5c4ad6ccc4bb377b4980
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-18 13:07:10 +0300

    dsy

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -81,11 +81,11 @@ class MainPanel(MenuPanel):
                 self._printer.get_dev_stat(x, "temperature"),
                 self._printer.get_dev_stat(x, "target")
             )
-        # for h in self._printer.get_heaters():
-        #     self.update_temp(
-        #         h,
-        #         self._printer.get_dev_stat(h, "temperature"),
-        #         self._printer.get_dev_stat(h, "target"),
-        #         None if h == "heater_bed" else " ".join(h.split(" ")[1:])
-        #     )   
+        for h in self._printer.get_heaters():
+            self.update_temp(
+                h,
+                self._printer.get_dev_stat(h, "temperature"),
+                self._printer.get_dev_stat(h, "target"),
+                None if h == "heater_bed" else " ".join(h.split(" ")[1:])
+            )   
         return
