ed3ef4f13d4ce4b64070f27f137e56c2c5c65200
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-18 14:27:36 +0300

    dd

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -34,17 +34,15 @@ class MainPanel(MenuPanel):
             i += 1
 
 
-        if self._printer.get_heaters():
-            self.labels["heater_bed"].set_label(
-                "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
-        # add_heaters = self._printer.get_heaters()
-        # for h in add_heaters:
-        #     if h == "heater_bed":
-        #         self.labels[h] = self._gtk.ButtonImage("bed", self._gtk.formatTemperatureString(0, 0))    
-        #     else:
-        #         name = " ".join(h.split(" ")[1:])
-        #         self.labels[h] = self._gtk.ButtonImage("heat-up", name)
-        #     self.heaters.append(h)
+        
+        add_heaters = self._printer.get_heaters()
+        for h in add_heaters:
+            if h == "heater_bed":
+                self.labels[h] = self._gtk.ButtonImage("bed", self._gtk.formatTemperatureString(0, 0))    
+            else:
+                name = " ".join(h.split(" ")[1:])
+                self.labels[h] = self._gtk.ButtonImage("heat-up", name)
+            self.heaters.append(h)
 
         i = 0
         cols = 3 if len(self.heaters) > 4 else (1 if len(self.heaters) <= 2 else 2)
