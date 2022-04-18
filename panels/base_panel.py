1c64d4389ab511ed9f87ed3471063ee335e9bc9e
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-18 13:11:50 +0300

    asd

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -141,14 +141,20 @@ class BasePanel(ScreenPanel):
 
         if self._printer.has_heated_bed():
             
-            add_heaters = self._printer.get_heaters()
-            for h in add_heaters:
-                if h == "heater_bed":
-                    self.labels[h] = self._gtk.ButtonImage("bed", self._gtk.formatTemperatureString(0, 0))    
-                else:
-                    name = " ".join(h.split(" ")[1:])
-                    self.labels[h] = self._gtk.ButtonImage("heat-up", name)
-                self.heaters.append(h)
+            heater_bed = self._gtk.Image("heat-up.svg", None, .4, .4)
+            self.labels['heat-up'] = Gtk.Label(label="20 C")
+            heater_bed_box = Gtk.Box(spacing=0)
+            heater_bed_box.pack_start(heater_bed, True, 5, 5)
+            heater_bed_box.pack_start(self.labels['heat-up'], True, 3, 3)
+            self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
+        
+            heater_bed = self._gtk.Image("bed.svg", None, .4, .4)
+            self.labels['heater_bed'] = Gtk.Label(label="20 C")
+            # self.labels['heater_bed'].get_style_context().add_class("printing-info")
+            heater_bed_box = Gtk.Box(spacing=0)
+            heater_bed_box.pack_start(heater_bed, True, 5, 5)
+            heater_bed_box.pack_start(self.labels['heater_bed'], True, 3, 3)
+            self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
 
            
             
@@ -187,14 +193,10 @@ class BasePanel(ScreenPanel):
 
         if action != "notify_status_update" or self._printer is None:
             return
-        for h in self._printer.get_heaters():
-            self.update_temp(
-                h,
-                self._printer.get_dev_stat(h, "temperature"),
-                self._printer.get_dev_stat(h, "target"),
-                None if h == "heater_bed" else " ".join(h.split(" ")[1:])
-            )   
-            
+        if self._printer.has_heated_bed():
+            self.labels["heater_bed"].set_label(
+                "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))    
+            self.labels["heat-up"].set_label("help")
                 
         for x in self._printer.get_tools():
             self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))     
