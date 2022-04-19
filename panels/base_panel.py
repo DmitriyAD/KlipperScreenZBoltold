3bc2febd8aa177bbfd52779ec65610f5800df54e
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-19 15:51:11 +0300

    d

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -150,8 +150,14 @@ class BasePanel(ScreenPanel):
             self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
 
         if self._printer.has_heated_bed():
+            heater_bed = self._gtk.Image("heat-up.svg", None, .4, .4)
+            self.labels['heat-up'] = Gtk.Label(label="20 C")
+            heater_bed_box = Gtk.Box(spacing=0)
+            heater_bed_box.pack_start(heater_bed, True, 5, 5)
+            heater_bed_box.pack_start(self.labels['heat-up'], True, 3, 3)
+            self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)
             
-        
+
             heater_bed = self._gtk.Image("bed.svg", None, .4, .4)
             self.labels['heater_bed'] = Gtk.Label(label="20 C")
             # self.labels['heater_bed'].get_style_context().add_class("printing-info")
@@ -194,7 +200,8 @@ class BasePanel(ScreenPanel):
         return self.layout
 
     def process_update(self, action, data): 
-
+        self.labels["heat-up"].set_label(
+                "%02d°" % round(self._printer.get_dev_stat("heat-up", "temperature")))
         if action != "notify_status_update" or self._printer is None:
             return
         
