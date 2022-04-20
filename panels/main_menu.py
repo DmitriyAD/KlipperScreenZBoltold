2ed03a108721fbbd49d0ab78863f03b57fd24b38
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-20 12:27:52 +0300

    d

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -40,7 +40,7 @@ class MainPanel(MenuPanel):
                 self.labels[h] = self._gtk.ButtonImage("bed", self._gtk.formatTemperatureString(0, 0))
             else:
                 name = " ".join(h.split(" ")[1:])
-                self.labels[h] = self._gtk.ButtonImage("heat-up", name)
+                self.labels[h] = self._gtk.ButtonImage("heat-up")
             self.heaters.append(h)
 
         i = 0
