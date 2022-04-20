a5151b88cd0a637c10f34daeb8cbdb91bd09cde0
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-20 15:07:32 +0300

    d

--- panels/preheat.py
+++ panels/preheat.py
@@ -66,7 +66,7 @@ class PreheatPanel(ScreenPanel):
         cooldown = self._gtk.ButtonImage('cool-down', _('Cooldown'), "color%d" % ((i % 4)+1))
         cooldown.connect("clicked", self._screen._confirm_send_action,
                          _("spasibo"), self.set_temperature, "cooldown")
-        cooldown.set_vexpand(False)                 
+        cooldown.set_vexpand(True)                 
 
         row = int(i/2) if i % 2 == 0 else int(i/2)+1
         self.labels["control_grid"].attach(cooldown, i % 2, int(i/2), 1, 1)
