5d1b3ac1a32eac66d5d1b8e8a0052351b88bada0
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-21 17:51:47 +0300

    f

--- panels/preheat.py
+++ panels/preheat.py
@@ -63,9 +63,8 @@ class PreheatPanel(ScreenPanel):
             i += 1
 
         cooldown = self._gtk.ButtonImage('cool-down', _('Cooldown'), "color%d" % ((i % 4)+1))
-        # cooldown.connect("clicked",self.set_temperature, "cooldown")
+        # cooldown.connect("clicked",self.set_temperature, "cooldown"
         cooldown.connect("clicked" ,self._screen._confirm_send_action,_("HELP!?"), "cooldown")
-        
                        
         
         
