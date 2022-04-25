0be79da71790f31c367fb6c56abac5fba1eb1748
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-25 12:09:50 +0300

    test

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -139,12 +139,12 @@ class BasePanel(ScreenPanel):
         if self._printer.has_heated_bed():
 
             heat_up = self._gtk.Image("heat-up.svg", None, .4, .4)
-            self.labels['heater_bed'] = Gtk.Label(label="20 C")
+            self.labels['heat-up'] = Gtk.Label(label="20 C")
             # self.labels['heater_bed'].get_style_context().add_class("printing-info")
-            heater_bed_box = Gtk.Box(spacing=0)
-            heater_bed_box.pack_start(heater_bed, True, 5, 5)
-            heater_bed_box.pack_start(self.labels['heat-up'], True, 3, 3)
-            self.control['temp_box'].pack_end(heater_bed_box, True, 3, 3)    
+            heat_up_box = Gtk.Box(spacing=0)
+            heat_up_box.pack_start(heat_up, True, 5, 5)
+            heat_up_box.pack_start(self.labels['heat-up'], True, 3, 3)
+            self.control['temp_box'].pack_end(heat_up_box, True, 3, 3)    
 
             heater_bed = self._gtk.Image("bed.svg", None, .4, .4)
             self.labels['heater_bed'] = Gtk.Label(label="20 C")
