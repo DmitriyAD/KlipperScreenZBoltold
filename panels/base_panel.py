1b577c2b1ef84a153d57d4fac870d84f055f6fda
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-26 12:34:20 +0300

    c

--- panels/base_panel.py
+++ panels/base_panel.py
@@ -192,8 +192,8 @@ class BasePanel(ScreenPanel):
         if self._printer.has_heated_bed():
             self.labels["heater_bed"].set_label(
                 "%02d°" % round(self._printer.get_dev_stat("heater_bed", "temperature")))
-            self.labels["heat_up"].set_label(
-                "%02d°" % round(self._printer.get_dev_stat("heat_up", "temperature_sensore")))          
+            # self.labels["heat_up"].set_label(
+            #     "%02d°" % round(self._printer.get_dev_stat("heat_up", "temperature_sensore")))          
         for x in self._printer.get_tools():
             self.labels[x].set_label("%02d°" % round(self._printer.get_dev_stat(x, "temperature")))
 
